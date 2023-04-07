696eaa7ed2e42e51c880d66f418b4bc00d119a0c
UGREP\Sysadmin <creeper_king@mail.ru>
2023-04-07 14:39:13 +0300

    fix test controller

--- src/main/java/com/helovei/restaurant/api/controller/TestController.java
+++ src/main/java/com/helovei/restaurant/api/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity<?> getTest(){
         return ResponseEntity.ok("A-OK");
     }
