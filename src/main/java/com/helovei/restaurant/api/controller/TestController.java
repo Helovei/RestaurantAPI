50f30aeb1b7d84ab3a58e72f6815bb9bfb24b75a
UGREP\Sysadmin <creeper_king@mail.ru>
2023-04-07 14:37:39 +0300

    add test controller

---
+++ src/main/java/com/helovei/restaurant/api/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.helovei.restaurant.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<?> getTest(){
+        return ResponseEntity.ok("A-OK");
+    }
+
+}
